698c42903763f7a7b66e34ec3f5d6ebc1c8920c9
Garvit Pugalia <garvitpugalia@gmail.com>
2023-06-13 01:45:42 -0700

    Update kitti-3d-3class.py

--- configs/_base_/datasets/kitti-3d-3class.py
+++ configs/_base_/datasets/kitti-3d-3class.py
@@ -122,8 +122,8 @@ train_dataloader = dict(
             box_type_3d='LiDAR',
             backend_args=backend_args)))
 val_dataloader = dict(
-    batch_size=2,
-    num_workers=1,
+    batch_size=4,
+    num_workers=2,
     persistent_workers=True,
     drop_last=False,
     sampler=dict(type='DefaultSampler', shuffle=False),
@@ -139,8 +139,8 @@ val_dataloader = dict(
         box_type_3d='LiDAR',
         backend_args=backend_args))
 test_dataloader = dict(
-    batch_size=2,
-    num_workers=1,
+    batch_size=4,
+    num_workers=2,
     persistent_workers=True,
     drop_last=False,
     sampler=dict(type='DefaultSampler', shuffle=False),
